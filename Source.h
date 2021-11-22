1ec51419e06b757c9806c4b599a40a43f469300e
Alex Freer <chasealex6@hotmail.com>
2021-11-22 14:25:38 -0500

    created header

---
+++ Source.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <freeglut.h>
+#include <FreeImage.h>
+
+class Particle
+{
+public:
+	Particle();
+	~Particle();
+
+private:
+
+};
+
+Particle::Particle()
+{
+}
+
+Particle::~Particle()
+{
+}
+
